9b2cd72cf0edbf177b480fb5676855c05431edd4
gabrielnascimentost <gabrielnascimenttto@outlook.com>
2019-10-04 22:06:03 -0300

    aplicacoes es6

--- curso_es6/src/main.js
+++ curso_es6/src/main.js
@@ -1,3 +1,4 @@
+/*
 class List {
     constructor(){
         this.data = [];
@@ -26,3 +27,70 @@ const MinhaLista = new TodoList();
 document.getElementById('novotodo').click = function(){
     MinhaLista.add('Novo todo');
 }
+*/
+import api from './api';
+
+class App{
+    constructor(){
+        this.repositories = [];
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
+        this.inputEl = document.querySelector('input[name=repository]')
+        this.registerHandlers();
+    }
+
+    registerHandlers(){
+        this.formEl.onsubmit = (event) => this.addRepository(event)
+    }
+
+    async addRepository(event){
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length ===0)
+            return;
+
+        const response = await api.get(`/repos/${repoInput}`);
+ 
+        const {name, description, html_url, owner: {avatar_url}} = response.data
+        
+        this.repositories.push({
+            name,
+            description,
+            avatar_url,
+            html_url,
+        });
+        this.render();
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.setAttribute('target', '_blank');
+            descriptionEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+            
+            this.listEl.appendChild(list);
+        });
+    }
+
+}
+
+new App();
